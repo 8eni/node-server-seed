9242905b4c480e81fb73cee9c276d6f6fb0012af
Benni Russell <bennirus@Sarahs-MacBook-Pro.local>
2018-02-05 18:07:11 +0000

    adds DELETE request

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,19 @@
 module.exports = function(app, db) {
+
+	var ObjectID = require('mongodb').ObjectID;
+
+	app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({ 'error': 'an error has occured' });
+			} else {
+				res.send(item);
+			}
+		})
+	});
+
   app.post('/notes', (req, res) => {
 		const note = {
 			text: req.body.body,
@@ -9,6 +24,18 @@ module.exports = function(app, db) {
 				res.send({ 'error': 'an error has occured' });
 			} else {
 				res.send(result.ops[0]);
+			}
+		})
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const note = { '_id': new ObjectID(id) };
+		db.collection('notes').remove(note, (err, result) => {
+			if (err) {
+				res.send({ 'error': 'an error has occured' });
+			} else {
+				res.send('Note '+ id +' deleted');
 			}
 		})
   });
