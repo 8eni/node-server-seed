57b208d7674adc271aeedd2741f69a226dbbdd79
Benni Russell <bennirus@Sarahs-MacBook-Pro.local>
2018-02-07 21:55:38 +0000

    adds index file with simple XHR

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const db             = require('./config/db');
 
 const app            = express();
 
-const port = 8000;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
